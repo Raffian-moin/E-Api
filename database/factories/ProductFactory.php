8d8822a8b9f1dae915a0a6e64046c57ee62e5de5
Raffian-moin <raffianmoin69@gmail.com>
2019-10-14 23:25:05 +0600

    faker creation

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(product::class, function (Faker $faker) {
     return [
-        //
+        
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,1000),
+        'stock'=>$faker->numberBetween(50,100),
+        'discount'=>$faker->numberBetween(5,30)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\review;
+use App\Model\product;
 use Faker\Generator as Faker;
 
 $factory->define(review::class, function (Faker $faker) {
     return [
-        //
+    	'product_id'=>function(){
+    		return product::all()->random();
+    	},
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(1,5)
     ];
 });
