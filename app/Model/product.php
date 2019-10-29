aad5cf7b447323eb6d867923092968a76f650ae7
Raffian-moin <raffianmoin69@gmail.com>
2019-10-29 21:46:12 +0600

    update product in controller and postman

--- app/Model/product.php
+++ app/Model/product.php
@@ -8,7 +8,12 @@ use App\Model\review;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
+
 {
+	protected $fillable = [
+		'name','price','detail','discount','stock'
+	];
+
     public function reviews(){
     	return $this->hasMany('App\Model\review');
     }
